71dfe3aae67d91d9c93635dd531aae0f6b359f33
lvqang <lvqiang1208@163.com>
2020-05-07 09:10:53 -0700

    just for learning myself

--- class.cpp
+++ class.cpp
@@ -1,1 +1,115 @@
 #include"class.h"
+
+//---------------------QueryResult-------------------
+    hared_ptr< vector<string> > QueryResult::get_file()
+    {
+        return file;
+    }
+//-------------------QueryResult end-----------------
+
+//--------------------------------------TextQuery---------------------
+        TextQuery:: TextQuery(ifstream &is): file(new vector<string>)
+        {
+                string text;
+                while( getline(is, text) )//按行读入文本
+                {
+                        file->push_back(text);
+                        int no = file->size()-1;//行号
+                        istringstream line(text);//字符串流绑定每一行
+                        string word;
+                        while( line>>word )
+                        {
+                                auto &lines = wm[word];
+                                if( ! lines )
+                                {
+                                        lines.reset( new set<line_no> );//此处尝试不用reset函数
+                                }
+                                lines->insert(no);
+                        }
+                }
+        }
+
+
+QueryResult TextQuery::  query(const string &str) const
+{
+       // shared_ptr<set<line_no>> lines = wm[str];//使用下标运算会导致map创建新的成员
+        shared_ptr< vector<string> > files = file;
+        if(  wm.find(str) == wm.end() )
+        {
+                static shared_ptr< set<line_no> > liness = new set<line_no>;//静态变量，生存周期最长，防止指针无效
+                return QueryResult(str, liness, files);
+        }
+        else  return QueryResult(str, wm[str], files);
+}
+//----------------------------------TextQuery end--------------
+
+
+
+
+//--------------------------------------Query_base--------------------
+QueryResult OrQuery::eval(const TextQuery &text) const
+{
+        auto right = rhs.eval(text);
+        auto left   = lhs.eval(text);
+
+        auto ret_lines = make_shared< set<line_no> >( left.begin(), left.end() );
+
+        ret_lines->insert( right.begin(), right.end() );
+
+        return QueryResult( rep(), ret_lines, left.get_file() );
+}
+
+
+QueryResult AndQuery::eval(const TextQuery &text) const
+{
+        auto right = rhs.eval(text);
+        auto left   = lhs.eval(text);
+
+        auto ret_lines = make_shared< set<line_no> >( );
+        set_intersection( left.begin(), left.end(), right.begin(), right.end(), inserter( *ret_lines, ret_lines->begin() ) );
+
+        return QueryResult( rep(), ret_lines, left.get_file() );
+}
+
+QueryResult NotQuery::eval(const TextQuery &text) const
+{
+    auto result = query.eval(text);//WordQuery's eval
+
+    auto ret_lines = make_shared< set<line_no> >( );
+    auto left = make_shared< vector<string> >( );//creat string
+
+    auto beg = result.lines->begin(), end = result.lines->end();//取其中set尝试
+
+    auto sz = result.get_file()->size();//get vector<string>'s size计算得到所有行的个数？
+
+    for(size_t n = 0; n!=sz; ++n)//改变方法
+    {
+        if(beg == end || *beg != n)
+        {
+            ret_lines->insert(n);
+            left->push_back(*(text.file)[n]);
+        }
+        else if(beg != end)
+            ++beg;
+    }
+    return QueryResult( rep(), ret_lines, left );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- class.h
+++ class.h
@@ -2,65 +2,84 @@
 #include<string>
 #include<vector>
 #include<memory>
-//#include<>
+#include<fstream>
+#include<sstream>
+#include<set>
+#include<map>
 using namespace std;
+
+//-------------------------------QueryResult-----------------------------------------
+class QueryResult
+{
+    friend ostream& print(ostream&, const QueryResult&);
+public:
+    QueryResult(string s, shared_ptr< set<TextQuery::line_no> > p, shared_ptr< vector<string> > f):sought(s), lines(p), file(f) { }
+    shared_ptr< vector<string> > get_file();//return shared_ptr<vector>
+protected:
+
+private:
+    string sought;
+    shared_ptr< set<TextQuery::line_no> > lines;
+    shared_ptr< vector<string> > file;
+};
+
+
+//-------------------------------TextQuery-----------------------------------------
 class TextQuery
 {
 public:
-
+        using line_no = vector<string>::size_type;
+        TextQuery(ifstream&);
+        QueryResult query(const string&) const;
 protected:
 
 private:
-
+        shared_ptr< vector<string> > file;
+        map< string, shared_ptr< set<line_no> > > wm;
 };
 
-class QueryResult
+
+//-------------------------------Query-----------------------------------------
+class Query_base;
+class Query                               //作为接口类
 {
+        friend Query operator~(const Query&);
+        friend Query operator&(const Query&, const Query&);
+        friend Query operator|(const Query&, const Query&);
 public:
-
+        Query(const string&);
+        QueryResult eval(const TextQuery&t) const
+        {
+                return q->eval(t);
+        }
+        string rep() const
+        {
+                return q->rep();
+        }
+        ostream& operator<<(ostream &os, const Query &query)
+        {
+                return os << query.rep();
+        }
+   // shared_ptr< vector<string> > get_file();
 protected:
 
 private:
-
+        Query(shared_ptr<Query_base> query): q(query) {}
+        shared_ptr<Query_base> q;
 };
 
-class Query
-{
-    friend Query operator~(const Query&);
-    friend Query operator~(const Query&, const Query&);
-    friend Query operator~(const Query&, const Query&);
-public:
-    Query(const string&);
-    QueryResult eval(const TextQuery&t) const
-    {
-        return q->eval(t);
-    }
-    string rep() const
-    {
-        return q->rep();
-    }
-    ostream& operator<<(ostream &os, const Query &query)
-    {
-        return os << query.rep();
-    }
-protected:
-
-private:
-    Query(shared_ptr<Query_base> query): q(query) {}
-    shared_ptr<Query_base> q;
-};
-
-
+//-------------------------------Query_base-----------------------------------------
 class Query_base//
 {
-    friend class Query;
+        friend class Query;
 public:
 
 protected:
-
+        using line_no = TextQuery::line_no;
+        virtual ~Query_base() = default;
 private:
-    virtual QueryResult eval(const TextQuery&) const=0;
-    virtual string rep() const = 0;
+        virtual QueryResult eval(const TextQuery&) const=0;//调用查询函数
+        virtual string rep() const = 0;//返回查询关键词
 };
 
 
@@ -71,22 +90,22 @@ private:
 //===================================================
 class WordQuery:public Query_base
 {
-    friend class Query;
+        friend class Query;
 public:
 
 protected:
 
 private:
-    WordQuery(const string &s):query_word(s) {}//creat function
-    QueryResult eval(const TextQuery &t) const
-    {
-        return t.query(query_word);
-    }
-    string rep() const
-    {
-        return query_word;
-    }
-    string query_word;
+        WordQuery(const string &s):query_word(s) {}//creat function
+        QueryResult eval(const TextQuery &t) const
+        {
+                return t.query(query_word);
+        }
+        string rep() const
+        {
+                return query_word;
+        }
+        string query_word;
 };
 
 inline Query::Query(const string &s):q(new WordQuery(s)){}
@@ -111,10 +130,65 @@ private:
     }
     Query query;
 };
-inline Query operator~(const Query &operand)
+inline Query operator~(const Query &operand)//Query的友元，可以访问私有构造函数
 {
-    return shared_ptr<Query_base> (new NotQuery(operand));
+    return shared_ptr<Query_base> (new NotQuery(operand));//通过指针隐式转换为Query类
 }
+
+//===========================================
+//Function:与或的基类
+//
+//===========================================
+class BinaryQuery: public Query_base
+{
+public:
+
+protected:
+        BinaryQuery(const Query &l, const Query &r, string s): lhs(l), rhs(r), opSym(s) { }
+        string rep() const
+        {
+                return "(" + lhs.rep() + " " + opSym + " " + rhs.rep() + ")";
+        }
+        Query lhs, rhs;
+        string opSym;
+private:
+
+};
+
+//-------------------------------AndQuery-----------------------------------------
+class AndQuery: public BinaryQuery
+{
+        friend Query operator &(const Query&, const Query&);
+public:
+
+protected:
+
+private:
+        AndQuery(const Query &left, const Query &right): BinaryQuery(left, right, "&"){ }
+        QueryResult eval(const TextQuery&)const;
+};
+inline Query operator &(const Query &lhs, const Query &rhs)
+{
+        return  shared_ptr<Query_base>( new AndQuery(lhs, rhs) );
+}
+
+//-------------------------------OrQuery-----------------------------------------
+class OrQuery: public BinaryQuery
+{
+        friend Query operator|(const Query &, const Query &);
+public:
+
+protected:
+
+private:
+        OrQuery(const Query &left, const Query &right): BinaryQuery(left, right, "|"){ }
+        QueryResult eval(const TextQuery &) const;
+};
+inline Query operator |(const Query &lhs, const Query &rhs)
+{
+        return  shared_ptr<Query_base>( new OrQuery(lhs, rhs) );
+}
+
 
 
 
